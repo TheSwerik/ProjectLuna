d15eda8022e491e390302311287cb2e4e09b6379
TheSwerik <derswerik@gmail.com>
2019-11-19 08:30:01 +0100

    Test 1

--- desktop/src/de/swerik/luna/desktop/DesktopLauncher.java
+++ desktop/src/de/swerik/luna/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import de.swerik.luna.Luna;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "Project Luna";
+        config.title = "Project Luna2";
         config.width=1280;
         config.height=720;
         new LwjglApplication(new Luna(), config);
