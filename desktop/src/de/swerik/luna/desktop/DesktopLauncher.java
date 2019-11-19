d8a369cdfb3ed8fa35b0042be300398c631199df
TheSwerik <derswerik@gmail.com>
2019-11-19 11:13:46 +0100

    idk what ive changed

--- desktop/src/de/swerik/luna/desktop/DesktopLauncher.java
+++ desktop/src/de/swerik/luna/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Project Luna";
-        config.width=1280;
-        config.height=720;
+        config.width = 1280;
+        config.height = 720;
         new LwjglApplication(new Luna(), config);
     }
 }
