9235ecc0e6f1f9ea178fcd2dc50ebc2d1abec5db
TheSwerik <derswerik@gmail.com>
2019-11-19 08:34:15 +0100

    not ignore runconfig

--- desktop/src/de/swerik/luna/desktop/DesktopLauncher.java
+++ desktop/src/de/swerik/luna/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import de.swerik.luna.Luna;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "Project Luna2";
+        config.title = "Project Luna";
         config.width=1280;
         config.height=720;
         new LwjglApplication(new Luna(), config);
