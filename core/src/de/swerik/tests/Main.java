6aaad2d70d2a1c0f102bd6ed262b8a51193bc21b
TheSwerik <derswerik@gmail.com>
2019-11-20 10:04:24 +0100

    Test

--- core/src/de/swerik/tests/Main.java
+++ core/src/de/swerik/tests/Main.java
@@ -18,7 +18,7 @@ public class Main extends Game {
     TextureRegion[][] regions;
     Sprite sprite;
     testInputHandler inputHandler;
-    testAnimation runningAnimation;
+    testAnimation runningAnimation;//test
 
     TiledMap tm;
     TiledMapRenderer tmr;
