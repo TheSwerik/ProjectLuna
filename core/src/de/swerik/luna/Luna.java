08e74b148793614f593586d9bd6dfd9b2ec60b94
TheSwerik <derswerik@gmail.com>
2019-11-23 13:19:42 +0100

    basic gravity

--- core/src/de/swerik/luna/Luna.java
+++ core/src/de/swerik/luna/Luna.java
@@ -11,6 +11,8 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 
 public class Luna extends ApplicationAdapter {
     private SpriteBatch batch;
@@ -18,7 +20,7 @@ public class Luna extends ApplicationAdapter {
     private TextureRegion[][] regions;
     private Sprite sprite;
     private InputHandler inputHandler;
-    private  Animation runningAnimation;
+    private Animation runningAnimation;
 
     private TiledMap tm;
     private TiledMapRenderer tmr;
@@ -28,6 +30,9 @@ public class Luna extends ApplicationAdapter {
     private BitmapFont font;
 
     private ParticleEffect effect = new ParticleEffect();
+
+    private static World world;
+    private static Body body;
 
     @Override
     public void create() {
@@ -50,7 +55,20 @@ public class Luna extends ApplicationAdapter {
 
         effect.load(Gdx.files.internal("particles/TestFlame.p"), Gdx.files.internal("particles/"));
         effect.start();
-        effect.setPosition(200f,200f);
+        effect.setPosition(200f, 200f);
+
+        world = new World(new Vector2(0, -98f), true);
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(sprite.getX(), sprite.getY());
+        body = world.createBody(bodyDef);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(sprite.getWidth() / 2, sprite.getHeight() / 2);
+        FixtureDef fd = new FixtureDef();
+        fd.shape = shape;
+        fd.density = 10;
+        Fixture fixture = body.createFixture(fd);
+        shape.dispose();
 
         Gdx.input.setInputProcessor(inputHandler);
     }
@@ -61,7 +79,7 @@ public class Luna extends ApplicationAdapter {
         Gdx.gl.glClearColor(0.4f, 0.4f, 0.4f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         sprite.translateX(inputHandler.getMovement());
-        effect.setPosition(sprite.getX(),sprite.getY());
+        effect.setPosition(sprite.getX(), sprite.getY());
         if (!sprite.isFlipX()) {
             sprite.flip(inputHandler.isMovingLeft(), false);
         }
@@ -75,12 +93,15 @@ public class Luna extends ApplicationAdapter {
         camera.update();
         batch.setProjectionMatrix(camera.combined);
 
+        world.step(16, 5, 2);
+        sprite.setPosition(body.getPosition().x, body.getPosition().y);
+
         batch.begin();
         sprite.draw(batch);
         font.draw(batch, "Hello, this works.", 200, 200);
-        effect.draw(batch,Gdx.graphics.getDeltaTime());
+        effect.draw(batch, Gdx.graphics.getDeltaTime());
         batch.end();
-        if(effect.isComplete()){
+        if (effect.isComplete()) {
             effect.reset();
         }
     }
