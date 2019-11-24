02942a477ea4d6b14438b7aca7ce659a5649a74d
TheSwerik <derswerik@gmail.com>
2019-11-24 15:58:26 +0100

    Starting out

--- core/src/de/swerik/ForeignTest/ForeignGame.java
+++ core/src/de/swerik/ForeignTest/ForeignGame.java
@@ -1,14 +1,42 @@
 package de.swerik.ForeignTest;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
-public class ForeignGame extends Game {
-    public static final int WIDTH = 1280;
-    public static final int HEIGHT= 720;
+public class ForeignGame implements ApplicationListener {
+    public static int WIDTH = 1280;
+    public static int HEIGHT = 720;
+
+    public static OrthographicCamera cam;
 
     @Override
     public void create() {
+        WIDTH = Gdx.graphics.getWidth();
+        HEIGHT = Gdx.graphics.getHeight();
+
+        cam = new OrthographicCamera();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
 
     }
 
--- desktop/src/de/swerik/luna/desktop/DesktopLauncher.java
+++ desktop/src/de/swerik/luna/desktop/DesktopLauncher.java
@@ -4,15 +4,27 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.swerik.ForeignTest.ForeignGame;
 import de.swerik.MorpheusTest.Main;
+import de.swerik.luna.Luna;
 
 public class DesktopLauncher {
+    private static final byte test = 3;
+
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Project Luna";
         config.width = 1280;
         config.height = 720;
-//        new LwjglApplication(new Main(), config);
-        new LwjglApplication(new ForeignGame(), config);
-//        new LwjglApplication(new Luna(), config);
+        switch (test) {
+            case 1:
+                new LwjglApplication(new Luna(), config);
+                break;
+            case 2:
+                new LwjglApplication(new Main(), config);
+                break;
+            case 3:
+                config.resizable = false;
+                new LwjglApplication(new ForeignGame(), config);
+                break;
+        }
     }
 }
