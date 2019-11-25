0fa6912340320def72fcc8d9b8f51575af0c4d8d
TheSwerik <derswerik@gmail.com>
2019-11-25 12:00:43 +0100

    ANDROID DOESNT WORK

--- android/src/de/swerik/luna/AndroidLauncher.java
+++ android/src/de/swerik/luna/AndroidLauncher.java
@@ -3,13 +3,16 @@ package de.swerik.luna;
 import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.swerik.ForeignTest.ForeignGame;
+import de.swerik.MorpheusTest.Main;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 //		initialize(new Main(), config);
 		initialize(new Luna(), config);
-	}
+//        initialize(new ForeignGame(), config);
+    }
 }
