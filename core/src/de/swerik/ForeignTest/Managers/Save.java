189cd71b87c1d1b793582541f3bf50f2ca978131
TheSwerik <derswerik@gmail.com>
2019-11-30 18:26:05 +0100

    Save Files

---
+++ core/src/de/swerik/ForeignTest/Managers/Save.java
@@ -0,0 +1,45 @@
+package de.swerik.ForeignTest.Managers;
+
+import com.badlogic.gdx.Gdx;
+
+import java.io.*;
+
+public class Save {
+
+    public static GameData gd;
+
+    public static void save() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("saves/highscores.sav"))) {
+            out.writeObject(gd);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Gdx.app.exit();
+        }
+    }
+
+    public static void load() {
+        if (!saveFileExists()) {
+            init();
+        }
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("saves/highscores.sav"))) {
+            gd = (GameData) in.readObject();
+            in.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            Gdx.app.exit();
+        }
+    }
+
+    public static boolean saveFileExists() {
+        File f = new File("saves/highscores.sav");
+        return f.exists();
+    }
+
+    public static void init() {
+        gd = new GameData();
+        gd.init();
+        save();
+    }
+
+}
