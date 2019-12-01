6533087cb53b99437653786bae9c9c1a79a65371
TheSwerik <derswerik@gmail.com>
2019-12-01 18:44:09 +0100

    create red blocks

--- core/src/de/swerik/Box2D_Tutorial/handlers/Variables.java
+++ core/src/de/swerik/Box2D_Tutorial/handlers/Variables.java
@@ -6,10 +6,10 @@ public class Variables {
     public static final float PPM = 100; // 100 Pixels = 1 Meter
 
     //category bits
-    public static final short BIT_GROUND = 2;   // 0000 0000 0000 0010
-    public static final short BIT_PLAYER = 4;      // 0000 0000 0000 0100
-
-
+    public static final short BIT_PLAYER = 2;      // 0000 0000 0000 0100
+    public static final short BIT_RED = 4;   // 0000 0000 0000 0010
+    public static final short BIT_GREEN = 8;   // 0000 0000 0000 0010
+    public static final short BIT_BLUE = 16;   // 0000 0000 0000 0010
 
 
 //    public static final short BIT_BALL = 7;   // 0000 0000 0000 0111
