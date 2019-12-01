50f2a7a4ffc46d4aeb1eb20cca249e2b8c87524a
TheSwerik <derswerik@gmail.com>
2019-12-01 20:58:11 +0100

    Add CrystalCollision

--- core/src/de/swerik/Box2D_Tutorial/handlers/MyContactListener.java
+++ core/src/de/swerik/Box2D_Tutorial/handlers/MyContactListener.java
@@ -1,23 +1,24 @@
 package de.swerik.Box2D_Tutorial.handlers;
 
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.Array;
 
 public class MyContactListener implements ContactListener {
 
     private int numFootContacts;
 
+    private Array<Body> removeList;
+
+    public MyContactListener() {
+        super();
+        removeList = new Array<Body>();
+    }
+
     @Override
     public void beginContact(Contact contact) {
         Fixture fa = contact.getFixtureA();
         Fixture fb = contact.getFixtureB();
-
-        if (fa.getUserData() == null && fb.getUserData() != null) {
-            throw new NullPointerException(fb.getUserData() + " collided with something with null-ID.");
-        } else if (fa.getUserData() != null && fb.getUserData() == null) {
-            throw new NullPointerException(fa.getUserData() + " collided with something with null-ID.");
-        } else if (fa.getUserData() == null && fb.getUserData() == null) {
-            throw new NullPointerException("Two objects with null-ID collided.");
-        }
+        nullCheck(fa, fb);
 
         if (fa.getUserData().equals("foot")) {
             numFootContacts++;
@@ -25,20 +26,20 @@ public class MyContactListener implements ContactListener {
         if (fb.getUserData().equals("foot")) {
             numFootContacts++;
         }
+
+        if (fa.getUserData().equals("crystal")) {
+            removeList.add(fa.getBody());
+        }
+        if (fb.getUserData().equals("crystal")) {
+            removeList.add(fb.getBody());
+        }
     }
 
     @Override
     public void endContact(Contact contact) {
         Fixture fa = contact.getFixtureA();
         Fixture fb = contact.getFixtureB();
-
-        if (fa.getUserData() == null && fb.getUserData() != null) {
-            throw new NullPointerException(fb.getUserData() + " collided with something with null-ID.");
-        } else if (fa.getUserData() != null && fb.getUserData() == null) {
-            throw new NullPointerException(fa.getUserData() + " collided with something with null-ID.");
-        } else if (fa.getUserData() == null && fb.getUserData() == null) {
-            throw new NullPointerException("Two objects with null-ID collided.");
-        }
+        nullCheck(fa, fb);
 
         if (fa.getUserData().equals("foot")) {
             numFootContacts--;
@@ -71,4 +72,18 @@ public class MyContactListener implements ContactListener {
      *       postsolve
      *
      * */
+
+    private void nullCheck(Fixture fa, Fixture fb) {
+        if (fa.getUserData() == null && fb.getUserData() != null) {
+            throw new NullPointerException(fb.getUserData() + " collided with something with null-ID.");
+        } else if (fa.getUserData() != null && fb.getUserData() == null) {
+            throw new NullPointerException(fa.getUserData() + " collided with something with null-ID.");
+        } else if (fa.getUserData() == null && fb.getUserData() == null) {
+            throw new NullPointerException("Two objects with null-ID collided.");
+        }
+    }
+
+    public Array<Body> getRemoveList() {
+        return removeList;
+    }
 }
