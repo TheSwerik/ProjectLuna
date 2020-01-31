0c6d808ebca44117d725fba76581cb78f85c6b14
TheSwerik <derswerik@gmail.com>
2020-02-01 00:03:57 +0100

    Update to newer Versions

---
+++ android/src/main/java/de/swerik/luna/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package de.swerik.luna.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.swerik.luna.Luna;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+		initialize(new Luna(), configuration);
+	}
+}
---
+++ lwjgl3/src/main/java/de/swerik/luna/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,32 @@
+package de.swerik.luna.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import de.swerik.luna.Luna;
+
+/**
+ * Launches the desktop (LWJGL3) application.
+ */
+public class Lwjgl3Launcher {
+    public static void main(String[] args) {
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new Luna(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle(Luna.TITLE);
+        configuration.useOpenGL3(true, 3, 2);                // Use OpenGL3
+        configuration.setWindowedMode((int) Luna.V_WIDTH, (int) Luna.V_HEIGHT);
+//        configuration.setWindowPosition(-1, -1);                                                //Center  Screen
+        configuration.setWindowPosition(-1, 25);
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+//        configuration.setMaximized(true);
+//        configuration.useVsync(true);
+//        configuration.setResizable(false);
+        return configuration;
+    }
+}
