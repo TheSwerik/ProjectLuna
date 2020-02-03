4b0e0bf777681bd47df0affadc5c1f0b9ada202a
TheSwerik <derswerik@gmail.com>
2020-02-03 17:14:36 +0100

    add Systems

---
+++ core/src/main/java/de/swerik/luna/ecs/systems/ControlledMovementSystem.java
@@ -0,0 +1,38 @@
+package de.swerik.luna.ecs.systems;
+
+import com.badlogic.ashley.core.*;
+import com.badlogic.ashley.utils.ImmutableArray;
+import de.swerik.luna.Luna;
+import de.swerik.luna.ecs.components.PositionComponent;
+import de.swerik.luna.ecs.components.VelocityComponent;
+import de.swerik.luna.manager.LogManager;
+
+public class ControlledMovementSystem extends EntitySystem {
+    private final Luna app;
+    private ImmutableArray<Entity> entities;
+
+    private final ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);
+    private final ComponentMapper<VelocityComponent> vm = ComponentMapper.getFor(VelocityComponent.class);
+
+    public ControlledMovementSystem(Luna app) {
+        this.app = app;
+    }
+
+    public void addedToEngine(Engine engine) {
+        if (engine != null) {
+            entities = engine.getEntitiesFor(Family.all(PositionComponent.class, VelocityComponent.class).get());
+        }else {
+            app.logger.log("ControlledMovementSystem: Ashley Engine is null.", LogManager.ERROR);
+        }
+    }
+
+    public void update(float delta) {
+        for (Entity entity : entities) {
+            PositionComponent position = pm.get(entity);
+            VelocityComponent velocity = vm.get(entity);
+
+            position.x += velocity.velocity * delta;
+        }
+    }
+
+}
---
+++ core/src/main/java/de/swerik/luna/ecs/systems/RenderSystem.java
@@ -0,0 +1,40 @@
+package de.swerik.luna.ecs.systems;
+
+import com.badlogic.ashley.core.*;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import de.swerik.luna.Luna;
+import de.swerik.luna.ecs.components.PositionComponent;
+import de.swerik.luna.ecs.components.RenderableComponent;
+import de.swerik.luna.ecs.components.SpriteComponent;
+import de.swerik.luna.manager.LogManager;
+
+public class RenderSystem extends EntitySystem {
+    private final Luna app;
+    private final SpriteBatch batch;
+    private ImmutableArray<Entity> entities;
+
+    private final ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);
+
+    public RenderSystem(SpriteBatch batch, Luna app) {
+        this.batch = batch;
+        this.app = app;
+    }
+
+    public void addedToEngine(Engine engine) {
+        if (engine != null) {
+            entities = engine.getEntitiesFor(Family.all(RenderableComponent.class, SpriteComponent.class, PositionComponent.class).get());
+        } else {
+            app.logger.log("RenderSystem: Ashley Engine is null.", LogManager.ERROR);
+        }
+    }
+
+    public void update(float delta) {
+        for (Entity entity : entities) {
+            SpriteComponent sprite = entity.getComponent(SpriteComponent.class);
+            PositionComponent position = entity.getComponent(PositionComponent.class);
+            batch.draw(sprite.sprite.getTexture(), position.x, position.y);
+        }
+    }
+
+}
