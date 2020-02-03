fca1acb9803c6b77b55d77e502ee4779d877bb4b
TheSwerik <derswerik@gmail.com>
2020-02-03 21:19:43 +0100

    fix collison

--- core/src/main/java/de/swerik/luna/utils/Variables.java
+++ core/src/main/java/de/swerik/luna/utils/Variables.java
@@ -11,18 +11,18 @@ public class Variables {
 
     public static final short FRIENDLY_BITS                 = 0x0001;
     public static final short ENEMY_BITS                    = 0x0002;
-    public static final short LEVEL_BITS                    = 0x0003;
-    public static final short NEUTRAL_BITS                  = 0x0004;
+    public static final short LEVEL_BITS                    = 0x0004;
+    public static final short NEUTRAL_BITS                  = 0x0008;
 
     public static final short FRIENDLY_FOOT_SENSOR          = 0x0010;
     public static final short ENEMY_FOOT_SENSOR             = 0x0020;
-    public static final short FRIENDLY_RIGHT_SENSOR         = 0x0030;
-    public static final short FRIENDLY_RIGHT_UPPER_SENSOR   = 0x0040;
+    public static final short FRIENDLY_RIGHT_SENSOR         = 0x0040;
+    public static final short FRIENDLY_RIGHT_UPPER_SENSOR   = 0x0080;
 
     public static final short FRIENDLY_LEFT_SENSOR          = 0x0100;
     public static final short FRIENDLY_LEFT_UPPER_SENSOR    = 0x0200;
-    public static final short FRIENDLY_RIGHT_WALL_SENSOR    = 0x0300;
-    public static final short FRIENDLY_LEFT_WALL_SENSOR     = 0x0400;
+    public static final short FRIENDLY_RIGHT_WALL_SENSOR    = 0x0400;
+    public static final short FRIENDLY_LEFT_WALL_SENSOR     = 0x0800;
 
 
 }
